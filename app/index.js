38832d41b984b678ba4bf1c70a20eabb842a18db
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-03 05:27:31 +0430

    Imported functions

--- app/index.js
+++ app/index.js
@@ -1,3 +1,6 @@
+import persian from "./jsFunctions";
+import separateNumber from "./jsFunctions";
+
 const input = document.getElementById("input");
 const button = document.getElementById("btn");
 const screen = document.getElementById("screen");
