241b536bc48f5ee543a41e11bb05e755b7521fd5
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-03 05:34:26 +0430

    Demo works successfully

--- app/index.js
+++ app/index.js
@@ -1,6 +1,22 @@
-import persian from "./jsFunctions";
-import separateNumber from "./jsFunctions";
-
+// variables
 const input = document.getElementById("input");
 const button = document.getElementById("btn");
 const screen = document.getElementById("screen");
+
+// functions
+const persian = (number) => {
+  const persian = number.replace(/\d/g, (d) => "۰۱۲۳۴۵۶۷۸۹"[d]);
+  return persian;
+};
+function separateNumber(number) {
+  let newNumber = number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  newNumber = persian(newNumber);
+  return newNumber;
+}
+
+// events
+button.addEventListener("click", (e) => {
+  const number = input.value;
+  const newNumber = separateNumber(number);
+  screen.innerHTML = newNumber;
+});
--- app/jsFunctions.js
+++ app/jsFunctions.js
@@ -1,8 +1,8 @@
-export const persian = (number) => {
+const persian = (number) => {
   const persian = number.replace(/\d/g, (d) => "۰۱۲۳۴۵۶۷۸۹"[d]);
   return persian;
 };
-export function separateNumber(number) {
+export default function separateNumber(number) {
   let newNumber = number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   newNumber = persian(newNumber);
   return newNumber;
